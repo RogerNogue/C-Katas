6771635441eaad494b5ab923b3efba48c3713de9
rnogue <rogernogueb@gmail.com>
2025-04-11 17:52:57 +0200

    chore: setup Palindrome kata project

---
+++ Palindrome/PalindromeTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PalindromeTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

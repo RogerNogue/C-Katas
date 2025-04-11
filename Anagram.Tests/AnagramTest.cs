cc5bb9b05f29e518b9ad98907fa522ce10dd378f
rnogue <rogernogueb@gmail.com>
2025-04-11 17:19:09 +0200

    chore: added testing project to the solution

---
+++ Anagram.Tests/AnagramTest.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram.Tests;
+
+[TestClass]
+[TestSubject(typeof(Program))]
+public class AnagramTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}

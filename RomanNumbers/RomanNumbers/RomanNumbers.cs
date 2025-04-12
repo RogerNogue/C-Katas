5587ae1941691a2d760912ecc19d9e7db86161c9
rnogue <rogernogueb@gmail.com>
2025-04-12 10:29:29 +0200

    feat: roman number of 5

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -4,6 +4,10 @@ public class RomanMathematician
 {
     public string RomanNumberOf(int number)
     {
+        if (number == 1)
+            return "I";
+        if (number == 5)
+            return "V";
         return "I";
     }
 }

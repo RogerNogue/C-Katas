aa4b96298a2eb578c6fe1893c42f22873512ad3b
rnogue <rogernogueb@gmail.com>
2025-04-12 15:52:52 +0200

    refactor: created structure to store Roman values

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -2,6 +2,19 @@ namespace RomanNumbers;
 
 public class RomanMathematician
 {
+    private List<KeyValuePair<int, string>> romanElementsValue;
+
+    public RomanMathematician()
+    {
+        romanElementsValue = new List<KeyValuePair<int, string>>();
+        romanElementsValue.Add(new KeyValuePair<int, string>(1, "I"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(5, "V"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(10, "X"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(50, "L"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(100, "C"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(500, "D"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(1000, "M"));
+    }
     public string RomanNumberOf(int number)
     {
         if (number == 5)

ca48be2f939785a266f5192422ec906d958e5712
rnogue <rogernogueb@gmail.com>
2025-04-12 15:55:18 +0200

    refactor: reversed roman map structure

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -7,13 +7,13 @@ public class RomanMathematician
     public RomanMathematician()
     {
         romanElementsValue = new List<KeyValuePair<int, string>>();
-        romanElementsValue.Add(new KeyValuePair<int, string>(1, "I"));
-        romanElementsValue.Add(new KeyValuePair<int, string>(5, "V"));
-        romanElementsValue.Add(new KeyValuePair<int, string>(10, "X"));
-        romanElementsValue.Add(new KeyValuePair<int, string>(50, "L"));
-        romanElementsValue.Add(new KeyValuePair<int, string>(100, "C"));
-        romanElementsValue.Add(new KeyValuePair<int, string>(500, "D"));
         romanElementsValue.Add(new KeyValuePair<int, string>(1000, "M"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(500, "D"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(100, "C"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(50, "L"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(10, "X"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(5, "V"));
+        romanElementsValue.Add(new KeyValuePair<int, string>(1, "I"));
     }
     public string RomanNumberOf(int number)
     {
@@ -38,6 +38,7 @@ public class RomanMathematician
         string conversion =string.Empty;
         while (number > 0)
         {
+            for
             --number;
             conversion += "I";
         }

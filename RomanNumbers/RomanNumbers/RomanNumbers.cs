6486738c46d3bc0c99b2b1124dd51bed6aa0f2b8
rnogue <rogernogueb@gmail.com>
2025-04-12 16:00:16 +0200

    refactor: removed hardcoded sections turning into simple algorithm

--- RomanNumbers/RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers/RomanNumbers.cs
@@ -17,19 +17,6 @@ public class RomanMathematician
     }
     public string RomanNumberOf(int number)
     {
-        if (number == 5)
-            return "V";
-        if (number == 10)
-            return "X";
-        if (number == 50)
-            return "L";
-        if (number == 100)
-            return "C";
-        if (number == 500)
-            return "D";
-        if (number == 1000)
-            return "M";
-        
         return ConvertNumber(number);
     }
 
@@ -38,8 +25,14 @@ public class RomanMathematician
         string conversion =string.Empty;
         while (number > 0)
         {
-            --number;
-            conversion += "I";
+            for (int currentRomanIndex = 0; currentRomanIndex < romanElementsValue.Count; currentRomanIndex++)
+            {
+                if (romanElementsValue[currentRomanIndex].Key <= number)
+                {
+                    number -= romanElementsValue[currentRomanIndex].Key;
+                    conversion += romanElementsValue[currentRomanIndex].Value;
+                }
+            }
         }
         return conversion;
     }

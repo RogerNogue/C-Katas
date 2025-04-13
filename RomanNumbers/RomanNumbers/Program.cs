d566b196a16daafd865d8c120594fa717d03523b
rnogue <rogernogueb@gmail.com>
2025-04-13 11:05:41 +0200

    feat: adapted the main to be able to debug

--- RomanNumbers/RomanNumbers/Program.cs
+++ RomanNumbers/RomanNumbers/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RomanNumbers;
+// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        RomanMathematician mathematician = new RomanMathematician();
+        Console.WriteLine("Hello, World!");
+    }
+}

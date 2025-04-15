16f4dad7637878abf9626a69c5738e890574a052
rnogue <rogernogueb@gmail.com>
2025-04-15 15:30:59 +0200

    chore: added the tests file

---
+++ ManhattanDistance/ManhattanDistance.Tests/ManhattanUrbanPlannerTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using ManhattanDistance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManhattanDistance.Tests;
+
+[TestClass]
+[TestSubject(typeof(ManhattanUrbanPlanner))]
+public class ManhattanUrbanPlannerTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}

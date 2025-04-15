578c1d7fd111aeacc2e74ed2bc353f88f0aacc20
rnogue <rogernogueb@gmail.com>
2025-04-15 16:04:11 +0200

    test: added test of dist between 11 and 03

--- ManhattanDistance/ManhattanDistance.Tests/ManhattanUrbanPlannerTest.cs
+++ ManhattanDistance/ManhattanDistance.Tests/ManhattanUrbanPlannerTest.cs
@@ -40,4 +40,12 @@ public class ManhattanUrbanPlannerTest
         
         Assert.AreEqual(4, sut.Distance(new Coordinate(5, 4), new Coordinate(3, 2)));
     }
+
+    [TestMethod]
+    public void DistanceFrom11To03Is3()
+    {
+        ManhattanUrbanPlanner sut = new ManhattanUrbanPlanner();
+        
+        Assert.AreEqual(3, sut.Distance(new Coordinate(1, 1), new Coordinate(0, 3)));
+    }
 }

edf00850087777333372d820c98abda28b719516
rnogue <rogernogueb@gmail.com>
2025-04-15 15:40:28 +0200

    feat: implemented distance from 00 to 00

--- ManhattanDistance/ManhattanDistance/ManhattanUrbanPlanner.cs
+++ ManhattanDistance/ManhattanDistance/ManhattanUrbanPlanner.cs
@@ -1,6 +1,21 @@
 namespace ManhattanDistance;
 
+public class Coordinate
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Coordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
 public class ManhattanUrbanPlanner
 {
-    
+    public int Distance( Coordinate from, Coordinate to )
+    {
+        return 0;
+    }
 }

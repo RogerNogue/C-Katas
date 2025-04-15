c7641c0170c0e252b6614372cc86134ce2df86af
rnogue <rogernogueb@gmail.com>
2025-04-15 15:44:55 +0200

    test: added test of distance from 11 to 11

--- ManhattanDistance/ManhattanDistance.Tests/ManhattanUrbanPlannerTest.cs
+++ ManhattanDistance/ManhattanDistance.Tests/ManhattanUrbanPlannerTest.cs
@@ -24,4 +24,12 @@ public class ManhattanUrbanPlannerTest
         
         Assert.AreEqual(0, sut.Distance(new Coordinate(0, 0), new Coordinate(0, 0)));
     }
+
+    [TestMethod]
+    public void DistanceFrom11To11Is0()
+    {
+        ManhattanUrbanPlanner sut = new ManhattanUrbanPlanner();
+        
+        Assert.AreEqual(0, sut.Distance(new Coordinate(1, 1), new Coordinate(1, 1)));
+    }
 }

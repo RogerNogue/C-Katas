48a8acd8c95f79fd86d0098b131319ff13aeffa3
rnogue <rogernogueb@gmail.com>
2025-04-20 16:26:28 +0200

    chore: created solution

---
+++ MorningRoutineFromScratch/MorningRoutineFromScratch/RoutineAssistant.cs
@@ -0,0 +1,6 @@
+namespace MorningRoutineFromScratch;
+
+public class RoutineAssistant
+{
+    
+}
---
+++ MorningRoutineFromScratch/MorningRoutineFromScratch/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MorningRoutineFromScratch;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

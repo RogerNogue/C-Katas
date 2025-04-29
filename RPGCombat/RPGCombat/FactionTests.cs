26676ac9cb75d8dddc385470b699409df580ff5b
rnogue <rogernogueb@gmail.com>
2025-04-29 11:15:27 +0200

    test: moved propsTests to its own file

--- RPGCombat/RPGCombat/FactionTests.cs
+++ RPGCombat/RPGCombat/FactionTests.cs
@@ -2,19 +2,6 @@ using static RPGCombat.Character;
 
 namespace RPGCombat;
 
-public class PropsTests
-{
-    [Test]
-    public void PropCanBeDamaged()
-    {
-        Prop sut = Prop.Aprop(2000);
-        
-        ACharacter.Harm(sut, 500);
-        
-        Assert.AreEqual(sut.Health, 1500);
-    }
-}
-
 public class FactionTests
 {
     [Test]

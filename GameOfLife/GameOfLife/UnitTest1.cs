a702375138f14b830c47ace6af9fd2f408e43e48
RogerNogue <rogernogueb@gmail.com>
2025-11-18 17:21:52 +0100

    docs: documented how I'll do the next feature using smaller steps

--- GameOfLife/GameOfLife/UnitTest1.cs
+++ GameOfLife/GameOfLife/UnitTest1.cs
@@ -4,6 +4,11 @@ Any live cell with fewer than two live neighbours dies, as if caused by under-po
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+to do the reproduction, I'll split it into smaller steps:
+- Count the empty neighbors for each cell
+- Find The Living Neighbors of an empty cell
+- Whole feature
  */
 public class Tests
 {

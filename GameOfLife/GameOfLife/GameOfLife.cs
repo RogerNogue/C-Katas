c0796687599e1e11396d91333ac2c2430816802a
RogerNogue <rogernogueb@gmail.com>
2025-11-19 13:40:50 +0100

    refactor: moved Cell to file

--- GameOfLife/GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife/GameOfLife.cs
@@ -18,29 +18,3 @@ public class GameOfLife
         world.PassTurn();
     }
 }
-
-public readonly struct Cell
-{
-    public readonly uint X, Y;
-    public Cell(uint x, uint y)
-    {
-        X = x;
-        Y = y;
-    }
-
-    public bool IsNeighbor(Cell cell)
-    {
-        bool isNeighbor = false;
-        int dx = Math.Abs((int)(X - cell.X));
-        int dy = Math.Abs((int)(Y - cell.Y));
-        isNeighbor = isNeighbor || dx == 1 && dy == 1;
-        isNeighbor = isNeighbor || dx == 1 && dy == 0;
-        isNeighbor = isNeighbor || dx == 0 && dy == 1;
-        return isNeighbor;
-    }
-
-    public static Cell Origin()
-    {
-        return new Cell(0, 0);
-    }
-}
